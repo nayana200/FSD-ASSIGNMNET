a90fb3de25959ff14c44eb6adaa8c22cc855de8c
nayana <nayanareddy2004@gmail.com>
2023-10-05 11:27:23 +0530

    assignment done

---
+++ assignment/ageprogram.js
@@ -0,0 +1,7 @@
+let age = 38
+if (age >= 18){
+    console.log("You age is eligible for Vote")
+}
+else{
+    console.log("Now Your not eligible to vote After the 18 years your eligible to vote")
+}
---
+++ assignment/sorcemarks.js
@@ -0,0 +1,19 @@
+let score= 55;
+
+if((score>=80)){
+console.log("Destinction")
+}
+else if((score>=60) && (score<=79)){
+    console.log("First Class")
+}
+else if((score>=50) && (score<=59)){
+    console.log("Second Class")
+}
+else if((score>=35) && (score<=45)){
+    console.log("Pass")
+}
+else{
+    console.log("Fail")
+}
+// o/p=Second Class
+
