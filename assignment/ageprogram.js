8aa1f4e95c670b5b7181703f85f1e1276eb6e8ec
nayana <nayanareddy2004@gmail.com>
2023-10-05 11:54:53 +0530

    html file created

--- assignment/ageprogram.js
+++ assignment/ageprogram.js
@@ -1,7 +1,9 @@
-let age = 38
+let age = parseInt(prompt("enter the age"))
 if (age >= 18){
     console.log("You age is eligible for Vote")
 }
 else{
     console.log("Now Your not eligible to vote After the 18 years your eligible to vote")
 }
+
+
