c6dcce43b7d14ac63bc22b4fe325501bb1dd2649
nayana <nayanareddy2004@gmail.com>
2023-11-06 10:51:26 +0530

    this practice folder

---
+++ practice/script.js
@@ -0,0 +1,29 @@
+// fetch("https://fakestoreapi.com/products")
+//   .then((data) => data.json())
+//   .then((objectdata) => {
+//     let titledata = "";
+//     objectdata.map((values) => {
+//       titledata = `${values.title}`;
+//       console.log(titledata);
+//     });
+//   })
+//   .catch((err) => console.log(err));
+// fetch("https://fakestoreapi.com/products")
+//   .then((data) => data.json())
+//   .then((objectdata) => {
+//     let tabletitle = "";
+//     objectdata.map((values) => {
+//       tabletitle += `${values.image}+${values.title}`;
+//       console.log(tabletitle);
+//     });
+//   });
+// fetch("https://fakestoreapi.com/products7678")
+//   .then((data) => data.json())
+//   .then((objectdata) => {
+//     let titledata = "";
+//     objectdata.map((values) => {
+//       titledata += `${values.title} '[values of an array]'${values.price}`;
+//       console.log(titledata);
+//     });
+//   })
+//   .catch((err) => console.log("error"));
